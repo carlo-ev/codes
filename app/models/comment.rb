5935c13b3a22a8f5aedcfa65f743e1756068f4e5
Carlo Espinal <carlo_ev@unitec.edu>
2012-06-07 23:46:53 -0600

    apparent error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ActiveRecord::Base
 	
   attr_accessible :content, :code_id, :created_at
   validates_presence_of :content
-	belongs_to :code, :user
+	belongs_to :code
+	belongs_to :user
 end
